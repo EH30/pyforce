845bb59f177f31c8190475aa5c1384fe013befbe
EH30 <games.eh2000@gmail.com>
2021-10-06 15:14:57 +0530

    Improvements and Bug Fixes

--- pyforce.py
+++ pyforce.py
@@ -37,14 +37,15 @@ def load_data(filename):
     except Exception:
         print("[-]error with storing data")
         print("[-]deleting old data")
-        opn.write("{}")
+        with open(filename, "w") as opn:
+            opn.write("{}")
         opn.close()
 
-def store_data(filename, hashed_value, h_type, val):
+def store_data(filename, hashed, h_type, val):
     global jdata
     try:
         with open(filename, "r+") as opn:
-            jdata[hashed_value] = [h_type, val]
+            jdata[hashed] = [h_type, val]
             opn.seek(0)
             json.dump(jdata, opn, indent=4)
             opn.truncate()
@@ -55,10 +56,27 @@ def store_data(filename, hashed_value, h_type, val):
         opn.write("{}")
         opn.close()
 
+def check_thash(hashed):
+    try:
+        thash = hash_length[len(hashed)]
+        return thash
+    except KeyError:
+        return None
+
 def is_cracked(data, hashed):
     if hashed in data:
         return data[hashed]
     return [None]
+
+def find_cracked(hashed, jdata, skip):
+    if skip != None and skip == 1:
+        return -1
+    else:
+        temp = is_cracked(jdata, hashed.lower())
+        if temp[0] != None:
+            return "[*]Found {0} || Cracked: [{1}] || hash: {2}".format(temp[0], temp[1], hashed.lower())
+    
+    return None
 
 def crypto(char_combo, algorithm):
     if algorithm == "md5":
@@ -87,7 +105,7 @@ def crypto(char_combo, algorithm):
         return hashlib.sha3_512(char_combo.encode("utf-8")).hexdigest()
 
 def combos(x, hashed, thash):
-    global Loop_Break, hashes, res
+    global Loop_Break, res
     
     for combo in itertools.product(''.join(strings), repeat=x):
         chars = ''.join(combo)
@@ -102,12 +120,35 @@ def combos(x, hashed, thash):
                 return 0
     return 1
 
+
+def wlist_crack(hashed, wlist, thash):
+    global Loop_Break, res
+    count = 0
+    with open(wlist, "r") as opn:
+        for line in opn:
+            sys.stdout.write("\r[*]Trying Line: %d"%(count))
+            sys.stdout.flush()
+            count += 1
+            if Loop_Break:
+                break
+
+            line_strip = line.strip()
+            for item in thash:
+                if crypto(line_strip, item).lower() == hashed:
+                    res = [item, line_strip]
+                    Loop_Break = True
+                    break
+                elif crypto(line, item).lower() == hashed:
+                    res = [item, line]
+                    Loop_Break = True
+                    break
+        opn.close()
+
 def launch_pad(hashed):
     global res, Loop_Break
     
-    try:
-        thash = hash_length[len(hashed)]
-    except KeyError:
+    thash = check_thash(hashed)
+    if thash == None:
         print("[-]error: Unknown Hash length")
         sys.exit()
 
@@ -168,12 +209,13 @@ if __name__ == "__main__":
         sys.exit()
 
     
-    parse = argparse.ArgumentParser(description="Brute Force Options")
+    parse = argparse.ArgumentParser(description="Options")
     parse.add_argument("-i", type=str, help="Enter The Hash")
-    parse.add_argument("-f", type=str, help="Enter The File Name Which Contains List Of hashes")
+    parse.add_argument("-f", type=str, help="Enter The File Name With List Of Hash (Example: list_of_hash.txt)")
+    parse.add_argument("-w", type=str, help="Enter The Wordlist ( If you use this then you can't use -t, -l and x )")
     parse.add_argument("-t", type=int, help="Enter The Number of Threads/max workers")
-    parse.add_argument("-l", type=int, help="Enter The First Length or single Length")
-    parse.add_argument("-x", type=int, help="Enter The Second Length")
+    parse.add_argument("-l", type=int, help="Enter The Starting Length")
+    parse.add_argument("-x", type=int, help="Enter The Ending Length")
     parse.add_argument("-s", type=int, help="-s 1 will skip checking for already cracked hash")
     args = parse.parse_args()
 
@@ -189,8 +231,14 @@ if __name__ == "__main__":
 
     if args.i != None and args.f != None or args.i == None and args.f == None:
         print("use -i for a single hash typed in the terminal and -f for a file containing list of hashes")
+        sys.exit()
     
-    if args.f != None:
+    if args.w != None:
+        if args.t != None or args.l != None or args.x != None:
+            print("if you're going to use -w wordlist then you can't use -t, -l and -x")
+            sys.exit()
+    
+    if args.f != None and args.w == None:
         if not os.path.isfile(args.f):
             print("[-]File does not exists")
             sys.exit()
@@ -204,13 +252,11 @@ if __name__ == "__main__":
                 data = line.strip()
                 if len(data) == 0:
                     continue
-                if args.s != None and args.s == 1:
-                    pass
-                else:
-                    temp = is_cracked(jdata, data.lower())
-                    if temp[0] != None:
-                        print("[*]Found {0} || Cracked: [{1}] || hash: {2}".format(temp[0], temp[1], data))
-                        continue
+                
+                temp = find_cracked(args.i, jdata, args.s)
+                if temp != -1 and temp != None:
+                    print(temp)
+                    continue
 
                 launch_pad(data.lower())
                 if res != None:
@@ -218,14 +264,58 @@ if __name__ == "__main__":
                 res = None
                 Loop_Break = False
         opn.close()
-    else:
-        if args.s != None and args.s == 1:
-            pass
-        else:
-            temp = is_cracked(jdata, args.i.lower())
-            if temp[0] != None:
-                print("[*]Found {0} || Cracked: [{1}] || hash: {2}".format(temp[0], temp[1], args.i.lower()))
+    elif args.w != None:
+
+        if args.i != None:
+            thash = check_thash(args.i)
+            if thash == None:
+                print("[-]error: Unknown Hash length")
                 sys.exit()
+            
+            temp = find_cracked(args.i, jdata, args.s)
+            if temp != -1 and temp != None:
+                print(temp)
+                sys.exit()
+            
+            wlist_crack(args.i, args.w, thash)
+            if res != None:
+                print("\n[*]Type: {0} || Hash Cracked: [{1}]".format(res[0], res[1]))
+                store_data(file_data, args.i, res[0], res[1])
+        else:
+            if not os.path.isfile(args.f):
+                print("[-]File does not exists")
+                sys.exit()
+            
+            if os.stat(args.f).st_size == 0:
+                print("[-]Empty File")
+                sys.exit()
+            
+            with open(args.f, "r") as opn:
+                for line in opn:
+                    line = line.strip()
+                    
+                    temp = find_cracked(line, jdata, args.s)
+                    if temp != -1 and temp != None:
+                        print(temp)
+                        continue
+
+                    thash = check_thash(line)
+                    if thash == None:
+                        print("[-]error: Unknown Hash length")
+                        sys.exit()
+                    
+                    wlist_crack(line, args.w, thash)
+                    if res != None:
+                        print("\n[*]Type: {0} || Hash Cracked: [{1}]".format(res[0], res[1]))
+                        store_data(file_data, line, res[0], res[1])
+                        Loop_Break = False
+                        res = None
+            opn.close()
+    else:
+        temp = find_cracked(args.i, jdata, args.s)
+        if temp != -1 and temp != None:
+            print(temp)
+            sys.exit()
         
         launch_pad(args.i.lower())
         if res != None:
